819a2dcf15819c711675bd1888303cc9d2ce0810
phil_c <npc1488@gmail.com>
2019-08-11 16:24:48 -0400

    Oauth FB/Google

---
+++ Handlers/StripeWebHookHandler.cs
@@ -0,0 +1,42 @@
+﻿using Stripe;
+using System.IO;
+using System.Web.Mvc;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.WebHooks;
+
+namespace DataStruct.Handlers
+{
+    public class StripeWebHookHandler: WebHookHandler
+    {
+
+        public StripeWebHookHandler()
+        {
+            this.Receiver = StripeWebHookReceiver.ReceiverName;
+        }
+
+        public override Task ExecuteAsync(string generator, WebHookHandlerContext context)
+        {
+            // For more information about Stripe WebHook payloads, please see 
+            // 'https://stripe.com/docs/webhooks'
+            StripeEvent entry = context.GetDataOrDefault<StripeEvent>();
+
+            // We can trace to see what is going on.
+            Trace.WriteLine(entry.ToString());
+
+            // Switch over the event types if you want to
+            switch (entry.EventType)
+            {
+                default:
+                    // Information can be returned in a plain text response
+                    context.Response = context.Request.CreateResponse();
+                    context.Response.Content = new StringContent(string.Format("Hello {0} event!", entry.EventType));
+                    break;
+            }
+
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ Json/Json.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataStruct.Json
+{
+
+    public class Json
+    {
+
+        public Web Web { get; set; }
+
+        public Json()
+        {
+            
+            
+        }
+
+        public static Json DeserializeJson(string jsonFile)
+        {
+
+
+            string data = Utility.Utility.GetFileData(@jsonFile);
+            var typeToDeserialize =  JsonConvert.DeserializeObject<Json>(data);
+
+            return typeToDeserialize;
+
+        }
+
+    }
+
+
+    public class Web
+    {
+
+        public string Client_Id { get; set; }
+        public string Project_Id { get; set; }
+        public string Auth_Uri { get; set; }
+        public string Token_Uri { get; set; }
+        public string Auth_provider_x509_cert_url { get; set; }
+        public string Client_secret { get; set; }
+        public IList<string> Redirect_uris { get; set; }
+        public IList<string> Javascript_origins { get; set; }
+
+
+       
+
+
+    }
+
+
+}
+
